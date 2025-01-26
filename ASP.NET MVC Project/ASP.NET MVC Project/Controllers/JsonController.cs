9d3ccb242a892e57851e9e8ef47909b45d2ad1b2
decodebymayank <mayankpal8424@gmail.com>
2025-01-26 09:57:11 +0530

    Lecture_no 38 ( Return Json From MVC Controller )

---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Controllers/JsonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_MVC_Project.Models;
+using Newtonsoft.Json;
+
+namespace ASP.NET_MVC_Project.Controllers
+{
+    public class JsonController : Controller
+    {
+        // GET: Json
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult JsonMethod()
+        {
+            JsonModel obj = new JsonModel()
+            {
+               Email = "omkaraob@gmail.com",
+               Name = "Omkara Oberoi"
+            };
+            var jsondata = JsonConvert.SerializeObject(obj);
+
+            return Json(jsondata,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ASP.NET MVC Project/ASP.NET MVC Project/Models/JsonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC_Project.Models
+{
+    public class JsonModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
